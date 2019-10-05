b1d97c127d44fd7091f5cc71f511a070198fa315
Hunter MacDonald <mactinite77@gmail.com>
2019-10-04 21:55:49 -0700

    starting on the weapon system

---
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class  WeaponBehaviour: MonoBehaviour
+{
+    public abstract void Attack(Sprite sprite, int comboCount);
+}
